dd2f6000ad6d0969713d41ce82662d7866980689
Mohamed Naji Aboo <42766301+NajiAboo@users.noreply.github.com>
2023-06-06 08:11:10 +0530

    Update config.py

--- llm/config.py
+++ llm/config.py
@@ -9,7 +9,7 @@ header_height = 60  # Main text distance from the top of the page: to remove hea
 footer_height = 540 # Remove footer
 start_page = 45
 end_page = 1055
-extraction_path = REPO_DIR / "llm/data/extracted_text.jsonl"
+extraction_path = "/content/drive/MyDrive/prognosis/prognosis.jsonl" #REPO_DIR / "llm/data/extracted_text.jsonl"
 
 # Text processing
 min_length = 100
